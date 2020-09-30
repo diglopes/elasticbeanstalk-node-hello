8c7f9b5d31c4b20f523e9443b736601579891ae7
Diego Lopes <diego-chez@hotmail.com>
2020-09-30 08:07:59 -0300

    add custom env variable

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const app = express()
 
 const PORT = process.env.PORT || 3000
+const envVar = process.env.TEST_ENV_VAR
 
 app.get("/", (req, res) => {
-    res.send("<h1>Hello world - Updated</h1>")
+    res.send(`
+        <h1>Hello world - Updated</h1>
+        <h2>Env var value: ${envVar}</h2>
+    `)
 })
 
 app.listen(PORT, () => {
